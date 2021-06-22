51f918bef9864ff83f9b8157caedba79739fa122
Naswih <51323070+neewtn@users.noreply.github.com>
2021-06-22 20:16:44 +0530

    Add files via upload

--- welcome.js
+++ welcome.js
@@ -2,7 +2,7 @@ const config = require("./config");
 const Canvas = require("canvas");
 const Discord = require("discord.js");
 const { registerFont, createCanvas } = require('canvas')
-registerFont('./Roboto-Regular.ttf', { family: 'Roboto' })
+registerFont('./RobotoCondensed-Bold.ttf', { family: 'Roboto' })
 
 module.exports = function (client) {
 
@@ -12,7 +12,7 @@ module.exports = function (client) {
         version: "4.8"
     }
     //log that the module is loaded
-    console.log(` :: ⬜️ Module: ${description.name} | Loaded version ${description.version} from ("${description.filename}")`)
+    console.log(` :: Module: ${description.name} | Loaded version ${description.version} from ("${description.filename}")`)
     //fires every time when someone joins the server
     client.on("guildMemberAdd", async member => {
       //If not in a guild return
@@ -26,62 +26,50 @@ module.exports = function (client) {
       ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
       ctx.strokeStyle = '#f2f2f2';
       ctx.strokeRect(0, 0, canvas.width, canvas.height);
-      //set the first text string 
+      //set the text strings 
       var textString3 = `${member.user.username}`;
       //if the text is too big then smaller the text
       if (textString3.length >= 14) {
         ctx.font = 'bold 100px "Roboto"';
         ctx.fillStyle = '#f2f2f2';
-        ctx.fillText(textString3, 720, canvas.height / 2 + 20);
-      }
-      //else dont do it
+        ctx.fillText(textString3, 620, canvas.height / 2 + 10);
+      }      
       else {
-        ctx.font = 'bold 150px "Roboto"';
+        ctx.font = 'bold 130px "Roboto"';
         ctx.fillStyle = '#f2f2f2';
-        ctx.fillText(textString3, 720, canvas.height / 2 + 20);
+        ctx.fillText(textString3, 620, canvas.height / 2 + 15);
       }
-      //define the Discriminator Tag
-      var textString2 = `#${member.user.discriminator}`;
-      ctx.font = 'bold 40px "Roboto"';
-      ctx.fillStyle = '#f2f2f2';
-      ctx.fillText(textString2, 730, canvas.height / 2 + 58);
-      //define the Member count
+
+      var textString4 = `Welcome to TECHSIAS`;
+      ctx.font = 'bold 60px "Roboto"';
+      ctx.fillStyle = '#bb86fc';
+      ctx.fillText(textString4, 560, canvas.height / 2 - 150);
+      
       var textString4 = `Member #${member.guild.memberCount}`;
       ctx.font = 'bold 60px "Roboto"';
-      ctx.fillStyle = '#f2f2f2';
-      ctx.fillText(textString4, 750, canvas.height / 2 + 125);
-      //get the Guild Name
-      var textString4 = `${member.guild.name}`;
-      ctx.font = 'bold 60px "Roboto"';
-      ctx.fillStyle = '#f2f2f2';
-      ctx.fillText(textString4, 700, canvas.height / 2 - 150);
+      ctx.fillStyle = '#bb86fc';
+      ctx.fillText(textString4, 650, canvas.height / 2 + 145);
       //create a circular "mask"
       ctx.beginPath();
       ctx.arc(315, canvas.height / 2, 250, 0, Math.PI * 2, true);//position of img
       ctx.closePath();
       ctx.clip();
       //define the user avatar
-      const avatar = await Canvas.loadImage(member.user.displayAvatarURL({ format: 'jpg' }));
+      const avatar = await Canvas.loadImage(member.user.displayAvatarURL({ format: 'png' }));
       //draw the avatar
       ctx.drawImage(avatar, 65, canvas.height / 2 - 250, 500, 500);
       //get it as a discord attachment
       const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'welcome-image.png');
       //define the welcome embed
       const welcomeembed = new Discord.MessageEmbed()
-        .setColor("RANDOM")
-        .setTimestamp()
-        .setFooter("Welcome", member.guild.iconURL({ dynamic: true }))
-        .setDescription(`**Welcome to ${member.guild.name}!**
-      Hi <@${member.id}>!, check your inbox!`)
+        .setColor("#4dff7a")
+        .setTitle(`**Welcome to TECHSIAS**`)
+        .setDescription(`Hi <@${member.id}>!\nCheck your inbox!`)
         .setImage("attachment://welcome-image.png")
         .attachFiles(attachment);
       //define the welcome channel
       const channel = member.guild.channels.cache.find(ch => ch.id === config.CHANNEL_WELCOME);
       //send the welcome embed to there
       channel.send(welcomeembed);
-      //member roles add on welcome every single role
-    //   let roles = config.ROLES_WELCOME;
-    //   for(let i = 0; i < roles.length; i++ )
-    //   member.roles.add(roles[i]);
     })
 }
